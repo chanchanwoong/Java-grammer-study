1ba569ba155c24f335aebacfead39b589a11145b
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-13 00:25:06 +0900

    Add: 자료형 학습

---
+++ src/com/javastudy/chapter2/Variable03.java
@@ -0,0 +1,13 @@
+package com.javastudy.chapter2;
+
+public class Variable03 {
+    public static void main(String args[]){
+        /**
+         * 자료형 : 기본형 + 참조형
+         *  기본형 : 실제 값을 저장, 실제 연산에 사용되는 것은 모두 기본형 변수
+         *      boolean, char, byte, short, int, long, float, double
+         *  참조형 : 어떤 값이 저장된 주소를 저장
+         *      8개의 기본형을 제외한 모든 타입
+         */
+    }
+}
