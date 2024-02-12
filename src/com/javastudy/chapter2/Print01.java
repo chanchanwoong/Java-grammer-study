2e89103d87deb41d51684990fa9d231eae76f972
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-12 23:45:18 +0900

    Add: print 학습

---
+++ src/com/javastudy/chapter2/Print01.java
@@ -0,0 +1,12 @@
+package com.javastudy.chapter2;
+
+public class Print01 {
+    public static void main(String args[]){
+        /**
+         * 화면에 글자 출력시에는 System.out.print() 또는 System.out.println() 또는 System.out.printf() 사용
+         *      print() : 괄호 안의 내용을 그대로 출력 후, 줄바꿈 하지 않음
+         *      println() : 괄호 안의 내용을 그대로 출력 후, 줄바꿈
+         *
+         */
+    }
+}
