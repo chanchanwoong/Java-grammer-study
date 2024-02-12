305d36d41e1b29373b8809c9995a18ee0df65146
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-12 23:54:26 +0900

    Add: 변수, 상수, 리터럴 개념 정리

---
+++ src/com/javastudy/chapter2/Variable01.java
@@ -0,0 +1,32 @@
+package com.javastudy.chapter2;
+
+public class Variable01 {
+    public static void main(String args[]){
+        /**
+         * 변수 : 하나의 값을 저장하는 공간
+         * 변수 선언 방법
+         *      변수타입 변수이름;
+         *
+         * 변수 타입
+         *      숫자_정수 : int(4바이트), long(8바이트)
+         *          20억이 넘었을 경우 long을 사용
+         *      숫자_실수 : float, double
+         *          float는 7자리, double은 15자리 정밀도의 차이를 보여줌
+         *      문자 : char
+         *      문자열 : String
+         *
+         * 상수 : 값을 초기화하면 다른 값으로 변경 불가, final 연산자를 사용
+         *      final int MAX_VALUE = 100;
+         *
+         * 리터럴 : 상수와 구분을 위해 사용된 용어
+         *
+         *
+         * 간단한 비교
+         *      변수 : 하나의 값을 저장하기 위한 공간
+         *      상수 : 값을 한번만 저장할 수 있는 공간
+         *      리터럴 : 그 자체로 값을 의미하는 것
+         *      int year(변수) = 2024(리터럴);
+         *      final int MAX_VALUE(상수) = 100(리터럴);
+         */
+    }
+}
