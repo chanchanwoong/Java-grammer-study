eff0b0514b5db3f9927c7776b393e696f56a387b
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-14 15:55:15 +0900

    Add: switch문

---
+++ src/com/javastudy/chapter4/Switch01.java
@@ -0,0 +1,24 @@
+package com.javastudy.chapter4;
+
+public class Switch01 {
+    public static void main(String args[]){
+        /**
+         * switch문
+         *  if문은 경우의 수가 많아지면 복작해지고 처리시간도 오래 걸린다.
+         *  그런 경우에는 switch문을 사용하는 것이 효율적이다.
+         *  조건식을 주고 결과와 일치하는 case문으로 이동한다.
+         *  이동한 case문 아래 문장들을 수행 후 break 문을 만나면 종료
+         *      보통 case문 끝에 break문을 넣는다.
+         *
+         *  switch (조건식){
+         *      case 값1 :
+         *          조건식 결과가 값1과 같은 경우 수행될 문장들
+         *      case 값2 :
+         *          조건식 결과가 값2와 같은 겨웅 수행될 문장들 또는 값1 케이스 끝난 후 수행될 문장들
+         *          break;
+         *      default :
+         *          조건식 결과가 case문이 없을 때 수행될 문장들
+         *
+         */
+    }
+}
