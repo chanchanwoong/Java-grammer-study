1eea8248a8b9ca8b6c48fb5b7895b765b18c5e1e
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-14 15:37:42 +0900

    Add: 조건 연산자

---
+++ src/com/javastudy/chapter3/Operator05.java
@@ -0,0 +1,12 @@
+package com.javastudy.chapter3;
+
+public class Operator05 {
+    public static void main(String args[]){
+        /**
+         * 조건 연산자
+         *  조건식, 식1, 식2 세 개의 피연산자를 필요로 하는 삼항 연산자이다.
+         *
+         *  조건식 ? 참일 경우 실행되는 식 : 거짓일 경우 실행되는 식
+         */
+    }
+}
