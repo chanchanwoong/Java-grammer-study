c1f099230733813cd89c0df8b02afee3191ecc9e
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-14 15:42:40 +0900

    Add: if

---
+++ src/com/javastudy/chapter4/If01.java
@@ -0,0 +1,14 @@
+package com.javastudy.chapter4;
+
+public class If01 {
+    public static void main(String args[]){
+        /**
+         * if 조건문
+         *  if (boolean 조건식){
+         *      조건식이 참일 경우 수행될 문장들
+         *  } else{
+         *      조건식이 거짓일 경우 수행될 문장들
+         *  }
+         */
+    }
+}
