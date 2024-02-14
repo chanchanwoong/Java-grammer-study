c368612ce4dd124c05c8507d42ead42826ae71ff
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-14 16:51:11 +0900

    Add: 배열 선언 및 생성

---
+++ src/com/javastudy/chapter5/Array01.java
@@ -0,0 +1,13 @@
+package com.javastudy.chapter5;
+
+public class Array01 {
+    public static void main(String args[]){
+        /**
+         * 배열
+         *  같은 타입의 여러 변수를 하나의 묶음으로 다루는 것
+         *  저장공간이 연속적으로 배치되어 있다.
+         *
+         *  int[] score = new int[5];
+         */
+    }
+}
