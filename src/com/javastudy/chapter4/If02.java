fbda9cc1f86c40c98472e3d5aee91b83f7c63a9e
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-14 15:45:14 +0900

    Add: if else if문

---
+++ src/com/javastudy/chapter4/If02.java
@@ -0,0 +1,18 @@
+package com.javastudy.chapter4;
+
+public class If02 {
+    public static void main(String args[]){
+        /**
+         * if 조건문
+         *  if (조건식1){
+         *      조건식1이 참일 경우 수행될 문장들
+         *  } else if (조건식2){
+         *      조건식1이 거짓인 것들 중 조건식2에서 참일 경우 수행될 문장들
+         *  } else if (조건식3){
+         *      조건식1과 조건식2에서 거짓인 것들 중 조건식3에서 참일 경우 수행될 문장들
+         *  } else{
+         *      모든 조건식이 거짓일 경우 수행될 문장들
+         *  }
+         */
+    }
+}
