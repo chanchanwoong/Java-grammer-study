a0bedebb118f31eb6aae661844fd0ee5db59892c
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-15 18:39:36 +0900

    Add: 제어자

---
+++ src/com/javastudy/chapter7/Modifier.java
@@ -0,0 +1,14 @@
+package com.javastudy.chapter7;
+
+public class Modifier {
+    /**
+     * 제어자
+     *  클래스, 변수 또는 메서드의 선언부에 함께 사용되어 부가적인 의미를 부여한다.
+     *  접근 제어자와 그 외 제어자로 나뉜다.
+     *  접근제어자를 제외한 그 외 제어자는 하나의 대상에 여러 제어자를 조합 가능하다.
+     *  접근 제어자 순서는 보통 접근 제어자를 제일 왼쪽에 위치한다.
+     *
+     *  접근제어자 : public, protected, default, private
+     *  그 외 : static, final, abstract, native, transient, synchronized, volatile, strictfp
+     */
+}
