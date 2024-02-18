84f144506689d08cf668f0871ff5b19c148d471a
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-18 23:44:57 +0900

    Add: java.lang 패키지

---
+++ src/com/javastudy/chapter9/JavaLangPackage01.java
@@ -0,0 +1,15 @@
+package com.javastudy.chapter9;
+
+public class JavaLangPackage01 {
+    /**
+     * java.lang 패키지는 java 프로그래밍에 가장 기본적인 클래스들이 모여있는 패키지 이다.
+     *  java.lang 패키지의 클래스는 import를 하지 않아도 사용 가능하다.
+     *  여러 클래스를 포함하고 있다.
+     *      1. Object 클래스 : 모든 클래스의 최상위 클래스
+     *      2. String, StringBuffer, StringBuilder : 문자열 관련 클래스
+     *      3. System : 화면에 값 출력할 때 사용하는 클래스
+     *      4. Math : 수학 관련 클래스
+     *      5. Thread : 쓰레드 관련 클래스
+     *      ,,,
+     */
+}
