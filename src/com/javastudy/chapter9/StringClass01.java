76074c7848eab0f017f5c372e2bf28867042cd41
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-19 01:01:04 +0900

    Add: String클래스

---
+++ src/com/javastudy/chapter9/StringClass01.java
@@ -0,0 +1,12 @@
+package com.javastudy.chapter9;
+
+public class StringClass01 {
+    /**
+     * String 클래스
+     *  String 클래스 앞에는 final이 붙어 있어 조상클래스가 될 수 없다.
+     *      그렇기에, 문자열 결합될 시에는 새로운 String 인스턴스가 생성되는 것 이다.
+     *      문자열 연산은 메모리공간을 많이 차지하므로 줄이는 것이 좋다.
+     *      문자열 연산이 많이 필요한 경우 StringBuffer 클래스를 사용하는 것이 좋다.
+     *          StringBuffer 클래스를 이용하면 하나의 인스턴스로 문자열 다루는 것이 가능하기 때문이다.
+     */
+}
