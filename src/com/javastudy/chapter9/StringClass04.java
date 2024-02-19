981ce489b37de07ab1e9804f6ec30138a44f6d5a
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-19 18:02:23 +0900

    Add: 문자열 기본형 변환

---
+++ src/com/javastudy/chapter9/StringClass04.java
@@ -0,0 +1,28 @@
+package com.javastudy.chapter9;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class StringClass04 {
+    /**
+     * 문자열과 기본형 변환
+     *  문자열에서 기본형으로 변환, parse 붙힌 메서드를 쓴다.
+     *  Boolean.parseBoolean(String s)
+     *  Byte.parseByte(String s)
+     *  Short.parseShort(String s)
+     *  Integer.parseInt(String s)
+     *  Long.parseLong(String s)
+     *  Float.parseFloat(String s)
+     *  Double.parseDouble(String s)
+     *
+     *  기본형에서 문자열로 변환, 숫자에는 빈 문자열 ""을 더해주거나, valueOf() 메서드를 쓴다.
+     *  String String.valueOf(boolean b)
+     *  String String.valueOf(char c)
+     *  String String.valueOf(int i)
+     *  String String.valueOf(long l)
+     *  String String.valueOf(float f)
+     *  String String.valueOf(double d)
+     */
+    public static void main(String args[]){
+    }
+}
