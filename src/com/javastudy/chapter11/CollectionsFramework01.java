398a7243ff0b009b99e4df8b8e4494e72fc48229
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-21 22:46:13 +0900

    Add: 컬렉션 프레임워크

---
+++ src/com/javastudy/chapter11/CollectionsFramework01.java
@@ -0,0 +1,20 @@
+package com.javastudy.chapter11;
+
+public class CollectionsFramework01 {
+    /**
+     * 컬렉션 프레임워크
+     *  다수의 데이터를 쉽고 효과적으로 처리할 수 있는 표준화된 방식을 제공하는 클래스 집합
+     *  컬렉션 프레임워크는 자바의 인터페이스를 사용해 구현된다.
+     *
+     *  종류
+     *  1. List 인터페이스
+     *      순서가 있는 데이터의 집합, 데이터의 중복을 허용
+     *      ArrayList, LinkedList, Stack, Vector 등
+     *  2. Set 인터페이스
+     *      순서를 유지하지 않는 데이터의 집합, 데이터의 중복을 허용하지 않는다.
+     *      HashSet, TreeSet 등
+     *  3. Map 인터페이스
+     *      키와 값의 쌍으로 이루어진 데이터의 집합, 순서는 유지되지 않으며, 키는 중복을 허용하지 않고, 값은 중복을 허용한다.
+     *      HashMap, TreeMap, Hashtable, Properties 등
+     */
+}
