43090b50a842b04601ba031822df973f98e7293a
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-21 22:51:09 +0900

    Add: List 인터페이스

---
+++ src/com/javastudy/chapter11/List01.java
@@ -0,0 +1,15 @@
+package com.javastudy.chapter11;
+
+public class List01 {
+    /**
+     * List 인터페이스
+     *  중복을 허용, 저장순서가 유지
+     *
+     *  상속계층도
+     *          List 인터페이스
+     *  |           |           |
+     *  Vector      ArrayList   LinkedList
+     *  |
+     *  Stack
+     */
+}
