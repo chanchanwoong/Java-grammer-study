d775244d3a0c2b96bb459841daf2143f8245543d
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-21 22:55:07 +0900

    Add: Map 인터페이스

---
+++ src/com/javastudy/chapter11/Map01.java
@@ -0,0 +1,16 @@
+package com.javastudy.chapter11;
+
+public class Map01 {
+    /**
+     * Map 인터페이스
+     *  키와 값을 하나의 쌍으로 묶어서 저장한다.
+     *  키의 중복은 허용하지 않지만, 값은 중복을 허용한다.
+     *
+     *  상속계층도
+     *   Set 인터페이스
+     *  |           |           |
+     *  SortedMap   HashMap     Hashtable
+     *  |           |
+     *  TreeMap     LinkedHashMap
+     */
+}
