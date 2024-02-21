3b9c78a1064141d0646745cc8b1a513c1c500541
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-21 22:52:44 +0900

    Add: Set 인터페이스

---
+++ src/com/javastudy/chapter11/Set01.java
@@ -0,0 +1,15 @@
+package com.javastudy.chapter11;
+
+public class Set01 {
+    /**
+     * Set 인터페이스
+     *  중복을 허용하지 않고, 저장순서가 유지되지 않는다.
+     *
+     *  상속계층도
+     *   Set 인터페이스
+     *  |           |
+     *  SortedSet   HashSet
+     *  |
+     *  TreeSet
+     */
+}
